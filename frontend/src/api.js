98c4f45cab70bc807b468e1ecf8bb8de7314893b
Swathi Raket <swathiraket@gmail.com>
2026-02-10 22:49:56 +0530

    Use VITE_API_URL for backend base URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://shopping-cart-backend.onrender.com/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use(config => {
